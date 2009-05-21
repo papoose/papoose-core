517a480b273b6fec319e42e096d2fcbb56304366
Alan D. Cabrera <adc@toolazydogs.com>
2009-05-21 23:10:48 +0000

    Intermediate checkin

---
+++ test-support/papoose-test-util/src/main/java/org/papoose/framework/mock/MockEmptyBundle.java
@@ -0,0 +1,147 @@
+/**
+ *
+ * Copyright 2008 (C) The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.papoose.framework.mock;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Dictionary;
+import java.util.Enumeration;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @version $Revision$ $Date$
+ */
+public class MockEmptyBundle implements Bundle
+{
+    public int getState()
+    {
+        return 0;
+    }
+
+    public void start(int i) throws BundleException
+    {
+    }
+
+    public void start() throws BundleException
+    {
+    }
+
+    public void stop(int i) throws BundleException
+    {
+    }
+
+    public void stop() throws BundleException
+    {
+    }
+
+    public void update() throws BundleException
+    {
+    }
+
+    public void update(InputStream inputStream) throws BundleException
+    {
+    }
+
+    public void uninstall() throws BundleException
+    {
+    }
+
+    public Dictionary getHeaders()
+    {
+        return null;
+    }
+
+    public long getBundleId()
+    {
+        return 0;
+    }
+
+    public String getLocation()
+    {
+        return null;
+    }
+
+    public ServiceReference[] getRegisteredServices()
+    {
+        return new ServiceReference[0];
+    }
+
+    public ServiceReference[] getServicesInUse()
+    {
+        return new ServiceReference[0];
+    }
+
+    public boolean hasPermission(Object o)
+    {
+        return false;
+    }
+
+    public URL getResource(String s)
+    {
+        return null;
+    }
+
+    public Dictionary getHeaders(String s)
+    {
+        return null;
+    }
+
+    public String getSymbolicName()
+    {
+        return null;
+    }
+
+    public Class loadClass(String s) throws ClassNotFoundException
+    {
+        return null;
+    }
+
+    public Enumeration getResources(String s) throws IOException
+    {
+        return null;
+    }
+
+    public Enumeration getEntryPaths(String s)
+    {
+        return null;
+    }
+
+    public URL getEntry(String s)
+    {
+        return null;
+    }
+
+    public long getLastModified()
+    {
+        return 0;
+    }
+
+    public Enumeration findEntries(String s, String s1, boolean b)
+    {
+        return null;
+    }
+
+    public BundleContext getBundleContext()
+    {
+        return null;
+    }
+}
